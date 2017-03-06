2d198ddf1c4f0fc554943ea5193123f93f13f725
Clément OUDOT <clem.oudot@gmail.com>
2017-03-06 18:06:25 +0100

    Remove leading sfl.local values

--- manifests/server/webserver/apache.pp
+++ manifests/server/webserver/apache.pp
@@ -13,23 +13,23 @@ class lemonldap::server::webserver::apache($webserverpath,$domain) {
 
 	augeas{"managerapache":
 		context	=> '/files/etc/lemonldap/manager-apache2.conf',
-		changes => 'set ServerName manager.sfl.local',
-		onlyif	=> 'values "ServerName" != "manager.sfl.local"',
+		changes => 'set ServerName manager.$domain',
+		onlyif	=> 'values "ServerName" != "manager.$domain"',
 	}
 	
 	augeas{"portalapache":
 		context	=> '/files/etc/lemonldap/portal-apache2.conf',
-		changes	=> 'set ServerName portal.sfl.local',
-		onlyif	=> 'values "ServerName" != "portal.sfl.local"',
+		changes	=> 'set ServerName portal.$domain',
+		onlyif	=> 'values "ServerName" != "portal.$domain"',
 	}
 
 	augeas{"testapache":
 		context	=> '/files/etc/lemonldap/test-apache2.conf',
 		changes	=> [
-			'set ServerName test1.sfl.local',
-			'set ServerAlias test2.sfl.local',
+			'set ServerName test1.$domain',
+			'set ServerAlias test2.$domain',
 			]
-		onlyif	=> 'values "ServerName" != "test1.sfl.local"',
+		onlyif	=> 'values "ServerName" != "test1.$domain"',
 	}
 	
 }
