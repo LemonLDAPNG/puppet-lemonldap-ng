a938a29b1dc2dad782c3525eca276447078d1c08
Clément OUDOT <clem.oudot@gmail.com>
2017-03-07 15:09:50 +0100

    Tidy file and add change default domain action

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,29 +1,37 @@
-class lemonldap::server ($domain,$webserver) {
-	# Execute OS specific actions
-	case $::osfamily {
-		'Debian':	{ include lemonldap::server::operatingsystem::debian($webserver) } 	
-		'RedHat':	{ include lemonldap::server::operatingsystem::redhat($webserver) }
-		default:
-	}
+class lemonldap::server($domain,$webserver) {
 
-	# LemonLDAP packages
-	$packageslemon = ['lemonldap-ng', 'lemonldap-ng-fr-doc']
+    # Execute OS specific actions
+    case $::osfamily {
+        'Debian':    { include lemonldap::server::operatingsystem::debian($webserver) }
+        'RedHat':    { include lemonldap::server::operatingsystem::redhat($webserver) }
+        default:
+    }
 
-	host{'lemonldap':
-		ip      => $::ipaddress,
-		aliases => ["reload.$lemonldap::domain"],
-	}
+    # LemonLDAP packages
+    $packageslemon = ['lemonldap-ng', 'lemonldap-ng-fr-doc']
 
-	package{'lemonpackages':
-		name 	=> "$packageslemon",
-		ensure	=> installed,
-		require	=> Package['lemonpackagewebserver'],
-	}
+    package{'lemonpackages':
+        name     => "$packageslemon",
+        ensure    => installed,
+        require    => Package['lemonpackagewebserver'],
+    }
 
-	case $webserver {
-                'apache':       { include lemonldap::server::webserver::apache($webserverpath,$domain) }
-                'nginx' :       { include lemonldap::server::webserver::nginx($webserverpath,$domain) }
-                default:
-        }
+    case $webserver {
+        'apache':       { include lemonldap::server::webserver::apache($webserverpath,$domain) }
+        'nginx' :       { include lemonldap::server::webserver::nginx($webserverpath,$domain) }
+        default:
+    }
+
+    # Set reload vhost in /etc/hosts
+    host{'lemonldap':
+        ip      => $::ipaddress,
+        aliases => ["reload.$lemonldap::domain"],
+    }
+
+    # Change default domain
+    exec{ 'change-default-domain':
+        command => "sed -i 's/example\.com/${domain}/g' /etc/lemonldap-ng/* /var/lib/lemonldap-ng/conf/lmConf-1.js /var/lib/lemonldap-ng/test/index.pl",
+        path    => ['/bin', '/usr/bin'],
+    }
 
 }
