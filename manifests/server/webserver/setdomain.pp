df55aac00d0b19046244335fde983dd770758ce2
Samuel Martin Moro <samuel.martin.moro@worteks.com>
2018-04-15 16:28:19 +0200

    fix(setdomain): expected apache2, found apache2.4, handle any suffix

--- manifests/server/webserver/setdomain.pp
+++ manifests/server/webserver/setdomain.pp
@@ -1,11 +1,13 @@
-define lemonldap::server::webserver::setdomain($domain    = $name,
-					       $webserver = "apache2") {
-    exec {
-	"Set LLNG $name Domain" :
-	    command => "sed -i 's/example\.com/$domain/g' ${name}-${webserver}.conf",
-	    cwd     => "/etc/lemonldap-ng",
-	    onlyif  => "grep example.com ${name}-${webserver}.conf",
-	    path    => "/usr/bin:/bin",
-	    require =>  Exec["Set LLNG Default Domain"];
+define lemonldap::server::webserver::setdomain($domain    = false,
+					       $webserver = "apache") {
+    if ($domain != false) {
+	exec {
+	    "Set LLNG $name Domain" :
+		command => "sed -i 's/example\.com/$domain/g' ${name}-${webserver}*.conf",
+		cwd     => "/etc/lemonldap-ng",
+		onlyif  => "grep example.com ${name}-${webserver}*.conf",
+		path    => "/usr/bin:/bin",
+		require =>  Exec["Set LLNG Default Domain"];
+	}
     }
 }
