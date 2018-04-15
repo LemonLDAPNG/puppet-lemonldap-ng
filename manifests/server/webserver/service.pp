c8c82e80127dd6de4d84a0a53497b1f35cb45659
Samuel Martin Moro <samuel.martin.moro@worteks.com>
2018-04-15 17:21:29 +0200

    fix(service): ensure enabled

--- manifests/server/webserver/service.pp
+++ manifests/server/webserver/service.pp
@@ -2,6 +2,7 @@ define lemonldap::server::webserver::service($service_name = $name) {
     if (! defined(Service[$service_name])) {
 	service {
 	    $service_name:
+		enable => true,
 		ensure => running;
 	}
     }
